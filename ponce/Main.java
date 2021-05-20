9f47784df1d874773e8ef7ca74c3b459c33e8684
Jonathan <pon20006@byui.edu>
2021-05-20 13:50:30 -0500

    Added my name

---
+++ ponce/Main.java
@@ -0,0 +1,8 @@
+package ponce;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, my name is DonJon.");
+        System.out.println("Hello, DonJon! My name is Jonathan");
+    }
+}
